1e9e82d28c431271efb7bd2866fcb1cf63bac244
Calamity-chain <78163718+Calamity-chain@users.noreply.github.com>
2021-02-09 21:02:51 +0100

    Created list of pokemons in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Bonbon aus Wurst';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Niki', height: 3, type:['cat','fluff','hypnosis']},
+  {name:'Patrick', height: 1, type:['bird','swear','fly']},
+  {name: 'zero', height: 5, type: ['dog','ghost','electric','float']},
+  {name: 'Lula', height: 0.5, type: ['spider','danse','poison']},
+  {name: 'janus', height: 0.1, type: ['leech','energySucker','jump']}
+];
